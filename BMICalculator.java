6f9354f949bdbf284b2a5562e4492a6bde6a6f30
Senamile N. Mzimela <148541085+Sena-Mzie@users.noreply.github.com>
2026-04-20 13:06:44 +0200

    Upload BMICalculator.java file
    A basic outline of the program, including a public class, basic imports and a main method with objects

---
+++ BMICalculator.java
@@ -0,0 +1,15 @@
+//import packages
+import java.util.Scanner;
+import java.util.Locale;
+
+//public class BMICalculator
+public class BMICalculator{
+
+//main method
+public static void main(String[] args){
+	//creating Scanner Object
+	Scanner sc = new Scanner(System.in);
+	Scanner.useLocale(Locale.US);
+}
+
+}
