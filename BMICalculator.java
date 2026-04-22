74eb0a9f40064a580a53c98f87ec824d1ec3d8b2
Senamile Mzimela <senamilenokuhle@gmail.com>
2026-04-22 13:13:05 +0200

    do-while loop in main method

--- BMICalculator.java
+++ BMICalculator.java
@@ -10,6 +10,24 @@ public static void main(String[] args){
 	//creating Scanner Object
 	Scanner sc = new Scanner(System.in);
 	Scanner.useLocale(Locale.US);
+	
+	char repeat = 0;
+
+	do{
+		int unitChoice = getUnitChoice(sc);
+
+		double weight = (unitChoice == 1)? getValidInput(sc, "Enter your weight in kilograms: ", 10, 600)
+		            : getValidInput(sc, "Enter your weight in pounds: ", 22, 1300);
+		
+		double height = (unitChoice == 1)? getValidInput(sc, "Enter your height in meters: ", 0.5, 2.5)
+		            : getValidInput(sc, "Enter your height in inches: ", 20, 100);
+		
+		double bmi = calculateBMI(unitChoice, weight, height);
+
+	//repeat = askToRepeat(sc);
+	System.out.print();
+
+	} while(repeat == 'Y' || repeat == 'y');
 }
 
 }
